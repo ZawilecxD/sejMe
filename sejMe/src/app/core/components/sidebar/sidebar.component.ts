24450a7c5034d6280ce6aec2ed73467f231914d1
maza <maza@ailleron.com>
2023-10-23 19:35:35 +0200

    removed duplicate terms fetch

--- sejMe/src/app/core/components/sidebar/sidebar.component.ts
+++ sejMe/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,20 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  inject,
-} from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable, map, startWith } from 'rxjs';
-import { ItemSelectOption } from 'src/app/shared/interface/ItemSelectOption';
-import { Term } from 'src/app/term/model/Term';
-import { loadTerms } from 'src/app/term/state/terms.actions';
-import {
-  selectAllTerms,
-  selectTermsError,
-  selectTermsStatus,
-} from 'src/app/term/state/terms.selectors';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'sm-sidebar',
@@ -22,21 +6,4 @@ import {
   styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent implements OnInit {
-  private readonly store = inject(Store);
-  private readonly router = inject(Router);
-  private readonly activeRoute = inject(ActivatedRoute);
-  terms$ = this.store.select(selectAllTerms);
-  status$ = this.store.select(selectTermsStatus);
-  error$ = this.store.select(selectTermsError);
-  readonly compareTerms = (a: Term, b: Term) => a?.num === b?.num;
-  activeTerm$ = this.activeRoute.data.pipe(map(data => data['term']));
-
-  ngOnInit() {
-    this.store.dispatch(loadTerms());
-  }
-
-  onTermSelect(term: Term) {
-    this.router.navigate([`/${term.num}`]);
-  }
-}
+export class SidebarComponent {}
--- sejMe/src/app/core/theme-picker/theme-picker.component.ts
+++ sejMe/src/app/core/theme-picker/theme-picker.component.ts
@@ -1,20 +1,32 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ThemeService } from '../utils/theme.service';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { AppTheme, ThemeService } from '../utils/theme.service';
 
 @Component({
   selector: 'sm-theme-picker',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="dropdown dropdown-left">
-      <label tabindex="0" class="btn btn-ghost" smIcon="paint-brush"></label>
+    <div class="dropdown dropdown-top dropdown-end w-full">
+      <label
+        #dropdownToggle
+        tabindex="0"
+        class="btn btn-ghost w-full rounded-none"
+        smIcon="paint-brush"
+        >{{ themeService.currentTheme }}</label
+      >
       <ul
         tabindex="0"
-        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box max-h-60 w-max">
+        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 max-h-60 w-max">
         <li *ngFor="let theme of themeService.availableThemes">
           <a
             class="btn btn-ghost content-center"
             [class.btn-active]="themeService.currentTheme === theme"
-            (click)="themeService.setTheme(theme)"
+            (click)="selectTheme(theme)"
             >{{ theme }}</a
           >
         </li>
@@ -24,4 +36,10 @@ import { ThemeService } from '../utils/theme.service';
 })
 export class ThemePickerComponent {
   themeService = inject(ThemeService);
+  @ViewChild('dropdownToggle') dropdownToggleEl!: ElementRef;
+
+  selectTheme(theme: AppTheme) {
+    this.dropdownToggleEl?.nativeElement.blur();
+    this.themeService.setTheme(theme);
+  }
 }
