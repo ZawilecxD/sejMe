f9816d5a489d312cfd089ca49b2790aff9d1360b
maza <maza@ailleron.com>
2024-04-07 15:23:12 +0200

    code cleanup

--- sejMe/src/app/term/api/term-api.service.ts
+++ sejMe/src/app/term/api/term-api.service.ts
@@ -12,39 +12,8 @@ export class TermApiService {
   private baseUrl = inject(BASE_API_URL);
 
   fetchList() {
-    // return this.http.get<Term[]>(`${this.baseUrl}/term`).pipe(
-    //   tap(terms => {
-    //     terms?.forEach(t => setTermLabel(t));
-    //   })
-    // );
     return this.http
       .get<{ terms: Term[] }>(`/assets/data/terms.json`)
       .pipe(map(response => response.terms));
   }
-
-  // fetchById(termNum: number) {
-  //   const term$ = this.http
-  //     .get<Term>(`${this.baseUrl}/term${termNum}`)
-  //     .pipe(tap(term => setTermLabel(term)));
-
-  //   return combineLatest([term$, this.fetchTermSittings(termNum)]).pipe(
-  //     tap(([term, sittings]) => {
-  //       term.sittings = sittings;
-  //     })
-  //   );
-  // }
-
-  // fetchTermSittings(termNum: number): Observable<TermSitting[]> {
-  //   return this.http
-  //     .get<Record<string, TermSitting>>(`/assets/term-sittings/${termNum}.json`)
-  //     .pipe(
-  //       map(sittings => {
-  //         return Object.keys(sittings).map(key => ({
-  //           ...sittings[key],
-  //           num: +key,
-  //         }));
-  //       }),
-  //       catchError(() => of([]))
-  //     );
-  // }
 }
