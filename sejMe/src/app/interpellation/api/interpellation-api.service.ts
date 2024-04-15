343e6a0c44a41d88afcc130eb12d8cab2d91ae06
maza <maza@ailleron.com>
2024-04-15 10:52:39 +0200

    interpellations API service, removed store code because it will not be used

---
+++ sejMe/src/app/interpellation/api/interpellation-api.service.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpUtilsService } from '../../shared/service/http-utils.service';
+import { Interpellation } from '../model/Interpellation';
+import { InterpellationSelectedFilters } from '../model/InterpellationSelectedFilters';
+
+@Injectable({ providedIn: 'root' })
+export class InterpellationApiService {
+  private readonly httpUtils = inject(HttpUtilsService);
+  private readonly http = inject(HttpClient);
+  private readonly apiPath = 'interpellations';
+
+  fetchList(term: number, filters: Partial<InterpellationSelectedFilters>) {
+    const filtersKEys = Object.keys(
+      filters
+    ) as (keyof InterpellationSelectedFilters)[];
+    const params = filtersKEys.reduce((httpParams, key) => {
+      return filters[key] ? httpParams.set(key, `${filters[key]}`) : httpParams;
+    }, new HttpParams());
+
+    return this.http.get<Interpellation[]>(
+      this.httpUtils.buildApiUrl(term, this.apiPath),
+      { params }
+    );
+  }
+}
